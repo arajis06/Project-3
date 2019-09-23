5ebb74a66aef933fe8f6c14fa4db67db84779f6d
ALEXIS RAJIS <arajis06@gmail.com>
2019-09-23 12:29:34 -0400

    heroku testing

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 //=== Dependencies ===//
 require('dotenv').config();
-const path = require("path");
 const express = require("express");
 const cors = require("cors");  
 const mongoose = require("mongoose");
 const passport = require("passport");
 const users = require("./routes/api/users");
-
 
 //=== Set Port ===//
 const PORT = process.env.PORT || 3001;
@@ -16,34 +14,34 @@ const app = express();
 
 //=== Configure middleware ===//
 // Parse request body as JSON
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-
-// Serve up static assets (usually on heroku)
-// if (process.env.NODE_ENV === "production") {
-// app.use(express.static("client/build"));
-// }
-app.use(express.static(path.join(__dirname, "client", "build")));
-
 //Cross-domain request
 app.use(cors());
 
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+app.use(express.static("client/build"));
+}
+// app.use(express.static(path.join(__dirname, "client", "build")));
+
+
 // DB config
-const db = require('./config/keys').MONGODB_URI;
+// const db = require('./config/keys').MONGODB_URI;
 //=== Database Setup ===//
-// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/bootcamp_hub";
+ const MONGODB_URI = process.env.MONGODB_URI || "mongodb://project3:project3@ds017678.mlab.com:17678/heroku_zb0s8d0f";
 
 //Connect to Mongo DB
-// mongoose.connect(MONGODB_URI)
-//     // mongoose
-//     // .connect(MONGODB_URI, { useNewUrlParser: true })
-//         .then(() => console.log("Connected to MongoDB"))
-//     .catch(err => console.log(err));
-// mongoose.connect(MONGODB_URI)
-    mongoose
-    .connect(db, { useNewUrlParser: true })
+//  mongoose.connect(MONGODB_URI);
+    mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
         .then(() => console.log("Connected to MongoDB"))
-    .catch(err => console.log(err));
+        .catch(err => console.log(err));
+// mongoose.connect(MONGODB_URI)
+
+    // mongoose
+    // .connect(db, { useNewUrlParser: true })
+    //     .then(() => console.log("Connected to MongoDB"))
+    // .catch(err => console.log(err));
     
 //=== Passport middleware ===//
 app.use(passport.initialize());
@@ -57,9 +55,9 @@ app.use("/api/users", users);
 // app.use('/', require('./routes/index'));
 // app.use('/', require('./routes/api/users'));
 
-app.get("*", (req,res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+// app.get("*", (req,res) => {
+//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// });
 // Start the server
 app.listen(PORT, () => {
     console.log("App running on port " + PORT + "!");
