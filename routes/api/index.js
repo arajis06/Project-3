41464600b7ef541bb4f8a2c0c6d6049c3671667e
Tsarina <tsarina.islam@gmail.com>
2019-09-23 21:18:53 -0400

    index.js in api folder

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,3 +1,16 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const router = express.Router();
+const path = require("path");
+const router = require("express").Router();
+const resources = require("./resources");
+const users = require("./users");
+
+// dem resource routes
+router.use("/resources", resourcesRoutes);
+// dem user routes
+router.use("/users", usersRoutes);
+
+// For anything else, render the html page
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+});
+
+module.exports = router;
