ec44bd0984d7fb6e7fd719b3def8d3e0310a905d
Tsarina <tsarina.islam@gmail.com>
2019-10-01 14:32:42 -0400

    some stuff

--- client/src/Components/PostArticle/PostArticle.js
+++ client/src/Components/PostArticle/PostArticle.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Form, Button} from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 import axios from 'axios';
 import './PostArticle.css'
 
@@ -7,49 +7,54 @@ class PostArticle extends React.Component {
 
   constructor() {
     super();
-     this.state = {
-       formFields: {name: ''}
-     }
-   };
-  
+    this.state = {
+      formFields: { name: '' }
+    }
+  };
 
-    render() {
-        return(
-<Form  onSubmit={this.formHandler(this.state.formFields)}>
+  formHandler = () => {
 
-  <Form.Group controlId="exampleForm.ControlInput1" name="topic">
-    <Form.Label>Topic</Form.Label>
-    <Form.Control type="text" placeholder="Programming" />
-  </Form.Group>
-  <Form.Group controlId="exampleForm.ControlInput2" name="title">
-    <Form.Label>Title</Form.Label>
-    <Form.Control type="text" placeholder="Programming 101" />
-  </Form.Group>
-
-  
-  <Form.Group controlId="exampleForm.ControlTextarea1"name="article">
-    <Form.Control as="textarea" rows="20" />
-  </Form.Group>
-  <Button variant="outline-warning" itemID="submit">Submit</Button>
-</Form>
-        )
-    };
-    formHandler(formFields) {
-
-      this.setState = {
-        formFields: {name: '', topic:'', title:'', article:''}
-      }
-      axios.post('/post', formFields)
-        .then(function(response){
-          console.log(response);
-          //Perform action based on response
+    // For after the submit
+    // this.setState = {
+    //   formFields: { topic: '', title: '', article: '', name: '' }
+    // }
+    axios.post('/api/post/', this.state.formFields)
+      .then(function (response) {
+        console.log(response);
+        //Perform action based on response
       })
-        .catch(function(error){
-          console.log(error);
-          //Perform action based on error
-        });
-      };
-  }
+      .catch(function (error) {
+        console.log(error);
+        //Perform action based on error
+      });
+  };
+
+  render() {
+    return (
+      <Form onSubmit={this.formHandler}>
+
+        <Form.Group controlId="exampleForm.ControlInput1" name="topic">
+          <Form.Label>Topic</Form.Label>
+          <Form.Control type="text" placeholder="Programming" />
+        </Form.Group>
+        <Form.Group controlId="exampleForm.ControlInput2" name="title">
+          <Form.Label>Title</Form.Label>
+          <Form.Control type="text" placeholder="Programming 101" />
+        </Form.Group>
+        <Form.Group controlId="exampleForm.ControlInput3" name="name">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" placeholder="John Smith" />
+        </Form.Group>
+
+        <Form.Group controlId="exampleForm.ControlTextarea1" name="article">
+          <Form.Control as="textarea" rows="20" />
+        </Form.Group>
+        <Button variant="outline-warning" type="submit">Submit</Button>
+      </Form>
+    )
+  };
+
+}
 
 
 export default PostArticle;
